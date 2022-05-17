ac1df2bf390ae04a7f8db7bac0df8badf7c8d4f3
wajeehaslam <ahsan.shuja@kwanso.com>
2022-05-17 16:37:28 +0500

    Working on the reservations

--- models/Reservation.js
+++ models/Reservation.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const reservationSchema = new mongoose.Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   bike: { type: Schema.Types.ObjectId, ref: "Bike" },
+  startTime: Date,
+  endTime: Date,
 });
 
 const Reservation = mongoose.model("Reservations", reservationSchema);
---
+++ routes/reservations.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const authGuard = require("../helpers/auth-guard");
+const roleGuard = require("../helpers/role-guard");
+const Bike = require("../models/Bike");
+const Reservation = require("../models/Reservation");
+const router = express.Router();
+
+router.get("/", authGuard, roleGuard("manager"), async function (req, res) {
+  try {
+    const reservations = await Reservation.paginate(
+      {},
+      { customLabels: { docs: "reservations" } }
+    );
+    res.status(200).send(bikes);
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message || "internal server error",
+    });
+  }
+});
+
+router.post("/", authGuard, roleGuard("user"), async function (req, res) {
+  try {
+    const { bikeId, startTime, endTime } = req.body;
+    await Reservation.create({
+      user: req.user._id,
+      bike: bikeId,
+      startTime,
+      endTime,
+    });
+    res.status(200).send({
+      message: "Bike reserved!",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message || "internal server error",
+    });
+  }
+});
+
+module.exports = router;
