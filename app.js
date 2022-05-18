56e6808b53e47b4b53a23fe743871f334d295220
wajeehaslam <wajeehaslam66@gmail.com>
2022-05-18 22:51:12 +0500

    [add reservation router ]

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const bikeRouter = require("./routes/bikes");
+const reservationRouter = require("./routes/reservations");
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/bikes", bikeRouter);
+app.use("/api/reservation", reservationRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
